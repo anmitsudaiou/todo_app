6f365ce04311cf2849ccd0218b5c4d5fd4061e2f
anmitsudaiou <yikor1504@gmail.com>
2022-03-10 18:29:25 +0900

    [WIP]タスク更新画面部分の処理を記述

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <div id="overRay">
+        <div id="contents">
+            <input type="text" /><br>
+            <input type="date"><br>
+            <button @click="update">更新</button>
+            <button @click="back">戻る</button>
+        </div>
+    </div>            
 </template>
 
 <script>
