6e5c66509ca6929ae505391ad7cff46d99fc68ec
anmitsudaiou <yikor1504@gmail.com>
2022-03-10 17:47:06 +0900

    [WIP]タスク表示エリアを作成

--- src/components/DisplayTask.vue
+++ src/components/DisplayTask.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>ここにタスクを表示</h1>
+    <div>
+        /* v-ifとDynamo使ってDataをとってくる*/
+        <h1>ここにタスクを表示</h1>
+        <button @click="editTask" id="edit">編集</button>
+        <button @click="deleteTask" id="delete">削除</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        editTask(){
+            //編集用コンポーネント利用
+        },
+        deleteTask(){
+            //JSのPrompt利用
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #edit {
+        color: aqua;
+        background-color: white;
+    }
+    #delete {
+        color: red
+    }
+</style>
+
